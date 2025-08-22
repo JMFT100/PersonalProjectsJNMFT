d7fb810189c5282f7e96fc023ab08c10fcf4831b
JMFT100 <71408164+JMFT100@users.noreply.github.com>
2025-08-22 16:48:06 +0200

    Changed bug in code
    Bug didn't run in background and would stop if terminal was closed or was interrupted

--- RubberScript.sh
+++ RubberScript.sh
@@ -1,12 +1,15 @@
+curl -sL "http://tinyurl.com/s63ve48" > rick.mp3
+
+
   for runner in mpg123 mpg321 ffplay mplayer vlc cvlc; do
     if command -v "$runner" > /dev/null 2>&1; then
       case "$runner" in
-        mpg123) exec nohup curl -sL "http://tinyurl.com/s63ve48" | mpg123 --loop -1 -; exit;;
-        mpg321) exec nohup curl -sL "http://tinyurl.com/s63ve48" | mpg321 --loop -1 -; exit;;
-        ffplay) exec nohup curl -sL "http://tinyurl.com/s63ve48" | ffplay -nodisp -autoexit -loop 0 -;exit;;
-        mplayer) exec nohup curl -sL "http://tinyurl.com/s63ve48" | mplayer -loop 0 -;exit;;
-        vlc) exec nohup curl -sL "http://tinyurl.com/s63ve48" | vlc --loop -;exit;;
-        cvlc) exec nohup curl -sL "http://tinyurl.com/s63ve48" | cvlc --loop -;exit;;
+        mpg123) nohup sh -c 'mpg123 --loop -1 rick.mp3' > /dev/null 2>&1 & exit;;
+        mpg321) nohup sh -c 'mpg321 --loop -1 rick.mp3' > /dev/null 2>&1 & exit;;
+        ffplay) nohup sh -c 'ffplay -nodisp -autoexit -loop 0 rick.mp3' > /dev/null 2>&1 & exit;;
+        mplayer) nohup sh -c 'mplayer -loop 0 rick.mp3' > /dev/null 2>&1 & exit;;
+        vlc) nohup sh -c 'vlc --loop rick.mp3' > /dev/null 2>&1 & exit;;
+        cvlc) nohup sh -c 'cvlc --loop rick.mp3' >/dev/null 2>&1 & exit;;
       esac
     fi
   done
